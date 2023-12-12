c8ad1351609190bc41039048a86447bc5b159187
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-13 00:35:58 +0500

    server error

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/water', (req, res) => {
+    console.log(`${req.ip} is asking for some water`)
+  res.send('Here is some water for you')
+})
+
+app.get('/food', (req, res) => {
+
+    console.log(`${req.ip} is asking for some food`)
+  res.send('Here is some water for you')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
